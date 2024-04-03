466765a45882b5e6b1d843f8cfffc77e8ad9d6a5
dipak140 <dipak@sariska.io>
2024-04-03 17:42:18 +0530

    Added a mut ref to jingle session

--- lib-gst-meet/src/conference.rs
+++ lib-gst-meet/src/conference.rs
@@ -970,8 +970,8 @@ impl StanzaFilter for JitsiConference {
                     {
                       let participantId = jid.node.clone().unwrap_or_default().to_string();
                       info!("Participant Left here: {:?}", participantId);
-                      if let Some(mut jingle_session) = self.jingle_session.lock().await.as_ref() {
-                        let mut map = &mut jingle_session.remote_ssrc_map;
+                      if let Some(jingle_session) = self.jingle_session.lock().await.as_ref() {
+                        let mut map = &jingle_session.remote_ssrc_map;
                         let mut sink_pad_name = "sdads";
 
                         map.retain(|_key, source| {
